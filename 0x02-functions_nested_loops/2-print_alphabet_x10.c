cb9f08a387e622e0aaf43047db91e588933a4f71
OjElsa <elsahojwang06@gmail.com>
2023-04-25 09:35:06 -0700

    create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - Print the alphabet 10 times
+ *Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int h;
+
+	for (h = 1; i <= 10; i++)
+	{
+		for (h = 97; h <= 122; h++)
+		{
+			_putchar(h);
+
+		}
+		_putchar('\n');
+	}
+}
