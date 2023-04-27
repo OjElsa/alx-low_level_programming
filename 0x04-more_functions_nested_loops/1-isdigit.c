58826e72be35e0d8c4f55130e65f46c9a4a6603b
OjElsa <elsahojwang06@gmail.com>
2023-04-27 09:22:40 -0700

    create.1-isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *@c: carrier integer values
+ *Return: 1 if true, 0 if false
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
