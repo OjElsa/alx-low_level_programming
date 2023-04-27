3a411d87b4c52497b3cc3802786bc633bf433ba0
OjElsa <elsahojwang06@gmail.com>
2023-04-27 09:35:21 -0700

    create.2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *mul(int a, int b)- gives the product of a ,b
+ *@a: variable
+ *@b: variable
+ *Return: c
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
