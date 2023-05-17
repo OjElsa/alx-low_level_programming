4e314b316e67dbe5887d4ab2373792c2cec7d1b5
OjElsa <elsahojwang06@gmail.com>
2023-05-17 16:08:58 -0700

    0-whatsmyname.c 2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,35 +1,16 @@
 #include <stdio.h>
-#include <string.h>
 #include "main.h"
 
 /**
-  *main - prints the name of the program
+ * main - prints the name of the program
  * @argc: number of arguments
  * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
-
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	char *program_name = argv[0];
-	char *last_slash = strrchr(program_name, '/');
+	printf("%s\n", *argv);
 
-	if (last_slash != NULL)
-	{
-		last_slash++;
-	}
-	else
-	{
-	last_slash = program_name;
-	}
-
-	for (int i = 0; last_slash[i] != '\0'; i++)
-	{
-	putchar(last_slash[i]);
-	}
-	putchar('\n');
-
-		return (0);
+	return (0);
 }
-
