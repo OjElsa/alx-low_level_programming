25c1fe4026033a7360fca16788ea9e80036e82e3
OjElsa <elsahojwang06@gmail.com>
2023-05-17 11:53:01 -0700

    Static Lib

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_isupper - checks the code
+ *@c: carrier integer values
+ *Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
