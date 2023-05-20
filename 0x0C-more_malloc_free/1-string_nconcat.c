ff63aed5626369120b9fd73cf7777d1022dcb4de
OjElsa <elsahojwang06@gmail.com>
2023-05-19 18:53:39 -0700

    a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,30 +1,53 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
+/**
+ * string_nconcat - function that concatenates two strings
+ * @s1: first char
+ * @s2: second char
+ * @n: unsigned int
+ * Return: NULL if funtion fails
+ */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int a, b, c;
+	char *s;
+
 	if (s1 == NULL)
-		s1 = "";
+	{
+		a = 0;
+	}
+	else
+	{
+		for (a = 0; s1[a]; ++a)
+			;
+	}
+
 	if (s2 == NULL)
-		s2 = "";
+	{
+		b = 0;
+	}
+	else
+	{
+		for (b = 0; s2[b]; ++b)
+			;
+	}
 
-	size_t s1_len = strlen(s1);
-	size_t s2_len = strlen(s2);
-
-	if (n >= s2_len)
-		n = s2_len;
-
-	char *result = malloc(sizeof(char) * (s1_len + n + 1));
-
-	if (result == NULL)
-	return (NULL);
-
-	strncpy(result, s1, s1_len);
-	strncpy(result + s1_len, s2, n);
-	result[s1_len + n] = '\0';
-
-	return (result);
+	if (b > n)
+	{
+		b = n;
+	}
+	s = malloc(sizeof(char) * (a + b + 1));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (c = 0; c < a; c++)
+	{
+		s[c] = s1[c];
+	}
+	for (c = 0; c < b; c++)
+	s[c + a] = s2[c];
+	s[a + b] = '\0';
+	return (s);
 }
-
