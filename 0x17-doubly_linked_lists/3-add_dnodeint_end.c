9aedb76da20fcac654cb1ddc6867e5dcd9201093
OjElsa <elsahojwang06@gmail.com>
2023-11-21 22:59:28 +0300

    code refactor

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,19 +1,37 @@
 #include "lists.h"
 
 /**
-* free_dlistint - frees a dlistint_t list
-* @head: ptr to the head of the linked list
-* Return: void
+* add_dnodeint_end - Adds a node at the end of a doubly linked list.
+* @head: Double pointer to the head of the linked list.
+* @n: Value to store in the new node
+*
+* Return: Address of the new element, NULL otherwise
 */
-
-void free_dlistint(dlistint_t *head)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *temp;
+	dlistint_t *new, *curr_ptr;
 
-	while (head)
+	if (!head)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		perror("malloc");
+		return (NULL);
 	}
+	new->n = n;
+	new->next = NULL;
+	if (!(*head))
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+	curr_ptr = *head;
+	while (curr_ptr->next)
+		curr_ptr = curr_ptr->next;
+	curr_ptr->next = new;
+	new->prev = curr_ptr;
+
+	return (new);
 }
