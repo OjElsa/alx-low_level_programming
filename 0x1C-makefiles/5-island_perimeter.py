e871d7c966f82edddbaac923e27fc24e75073fc5
OjElsa <elsahojwang06@gmail.com>
2024-01-24 16:01:12 -0800

    make file

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+
+""" perimeter of the island Module"""
+
+
+def island_perimeter(grid):
+    """Calculate the perimeter of an island
+    Args:
+        grid (list): List of list of integers
+    """
+    pass
